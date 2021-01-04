fa121392256966f974f89f22dbb2427b2caee653
Hunter Blaze <25360019+blazegeek@users.noreply.github.com>
2021-01-03 19:28:36 -0500

    change ' to "

--- scripts/listener.js
+++ scripts/listener.js
@@ -5,8 +5,8 @@
  */
 
 // Import Required Modules
-var events = require('events');
-var net = require('net');
+var events = require("events");
+var net = require("net");
 
 // Pool Listener Main Function
 var PoolListener = function (port) {
@@ -14,30 +14,30 @@ var PoolListener = function (port) {
     // Establish Log
     var _this = this;
     var emitLog = function(text) {
-        _this.emit('log', text);
+        _this.emit("log", text);
     };
 
     // Start Listener Server
     this.start = function() {
         net.createServer(function(c) {
-            var data = '';
+            var data = "";
             try {
-                c.on('data', function (d) {
+                c.on("data", function (d) {
                     data += d;
-                    if (data.slice(-1) === '\n') {
+                    if (data.slice(-1) === "\n") {
                         var message = JSON.parse(data);
-                        _this.emit('command', message.command, message.params, message.options, function(message) {
+                        _this.emit("command", message.command, message.params, message.options, function(message) {
                             c.end(message);
                         });
                     }
                 });
-                c.on('end', function () {});
-                c.on('error', function () {});
+                c.on("end", function () {});
+                c.on("error", function () {});
             }
             catch(e) {
                 emitLog(`CLI listener failed to parse message ${  data}`);
             }
-        }).listen(port, '127.0.0.1', function() {
+        }).listen(port, "127.0.0.1", function() {
             emitLog(`CLI listening on port ${  port}`);
         });
     };
